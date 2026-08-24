d10489216f376933805a5255e30245cdfed6f333
Dan <maratas.094747@gensan.sti.edu.ph>
2026-08-24 16:39:41 +0800

    Aug 24, 2025 4:39PM

---
+++ STREAM/Home/RCV/Scaler/Scaler_Home.swift
@@ -0,0 +1,18 @@
+//
+//  Scaler_Home.swift
+//  STREAM
+//
+//  Created by Dan on 8/24/26.
+//
+
+import SwiftUI
+
+struct Scaler_Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Scaler_Home()
+}
